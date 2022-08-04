71bc9f8a0bf14f88ae468ec0cca479cef201fc75
GHOSt <ghostdevbusiness@gmail.com>
2022-08-04 21:25:48 +0100

    fix: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+    plugins: [svelte()],
+
+    define: {
+        alias: [
+            {
+                find: '$lib',
+                replacement: resolve('./src/lib'),
+            },
+        ],
+    },
+});
