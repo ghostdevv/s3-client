cf09f7a53bcdfe992653fe8776a2a04e1db0d8c1
GHOST <ghostdevbusiness@gmail.com>
2022-08-04 23:28:30 +0100

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
     plugins: [svelte()],
 
-    define: {
+    resolve: {
         alias: [
             {
                 find: '$lib',
