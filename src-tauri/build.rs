5dc702d601172e4c4341960472bb2cc125df903d
GHOSt <ghostdevbusiness@gmail.com>
2022-08-05 17:20:59 +0100

    chore: update build script

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-  tauri_build::build()
+	// trigger recompilation when a new migration is added
+	println!("cargo:rerun-if-changed=migrations");
+
+	// Build Tauri
+	tauri_build::build()
 }
