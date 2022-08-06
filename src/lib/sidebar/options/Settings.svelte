aabab4607e580534a976a1140f61874a4d567e26
GHOSt <ghostdevbusiness@gmail.com>
2022-08-06 13:44:33 +0100

    feat: settings sidebar

--- src/lib/sidebar/options/Settings.svelte
+++ src/lib/sidebar/options/Settings.svelte
@@ -1,1 +1,56 @@
-settings
+<script lang="ts">
+    import { selectedBucket } from '$lib/buckets/buckets';
+
+    $: bucket = $selectedBucket;
+</script>
+
+<form>
+    <label>
+        Name
+
+        <input value={$selectedBucket.name} />
+    </label>
+
+    <label>
+        Bucket
+
+        <input value={$selectedBucket.bucket} />
+    </label>
+
+    <label>
+        Endpoint
+
+        <input value={$selectedBucket.endpoint} />
+    </label>
+
+    <label>
+        Region
+
+        <input value={$selectedBucket.region} />
+    </label>
+
+    <label>
+        Access Key ID
+
+        <input value={$selectedBucket.key_id} />
+    </label>
+
+    <label>
+        Access Key
+
+        <input value={$selectedBucket.key} />
+    </label>
+</form>
+
+<style lang="scss">
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        gap: 12px;
+    }
+
+    input {
+        margin-top: 8px;
+    }
+</style>
